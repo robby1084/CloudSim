83cfe425848f1bf2134c0c303e8b9d2b93b12222
chenjunda <374562607@qq.com>
2016-08-14 20:38:09 +0800

    singleton

--- ServerTerminal/src/org/iti/designpattern/Singleton.java
+++ ServerTerminal/src/org/iti/designpattern/Singleton.java
@@ -6,7 +6,7 @@ public class Singleton {
 	
 	private static class SingletonHolder{
 		private static final Singleton INSTANCE = new Singleton(); 
-	}
+	}                                                                 
 	
 	public static Singleton getInstance(){
 		return SingletonHolder.INSTANCE;
